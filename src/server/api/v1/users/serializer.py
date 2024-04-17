22e9dfae57970e3c2f951444862ea8639c886b44
Михаил Карпухин <mikekarpuhin@yandex.ru>
2024-04-17 15:52:19 +1000

    Права на изменения только для текущего пользователя

--- src/server/api/v1/users/serializer.py
+++ src/server/api/v1/users/serializer.py
@@ -1,61 +1,38 @@
 from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
-
 from rest_framework import serializers
-
 from server.apps.users.models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
-    """
-    Serializer пользователя
-    """
-
-    # Валидация пароля
     def _validate_password(self, value):
-        # Валидация пароля
         try:
             validate_password(value)
         except ValidationError as e:
             raise serializers.ValidationError(str(e))
         return value
 
-    # Валидация email
     def _validate_email(self, value, instance=None):
-        if instance is not None and instance.email == value:
+        if instance and instance.email == value:
             raise serializers.ValidationError("Новый email должен отличаться от старого")
         return value
 
     def create(self, validated_data):
         password = validated_data.pop('password')
-        user = User.objects.create_user(password=password, **validated_data)  # Передаем пароль в метод create_user
+        user = User.objects.create_user(password=password, **validated_data)
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.set_password(self._validate_password(password))
+        if 'email' in validated_data:
+            instance.email = self._validate_email(validated_data.pop('email'), instance)
+        return super().update(instance, validated_data)
 
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email', 'password', 'is_active', 'is_staff']
         extra_kwargs = {
-            'password': {'write_only': True},
-        }
-
-
-class UserUpdateSerializer(UserSerializer):
-    """
-    Serializer для обновления данных пользователя без пароля
-    """
-
-    # Обновление существующего пользователя
-    def update(self, instance, validated_data):
-        if 'password' in validated_data:
-            password = self._validate_password(validated_data.pop('password'))
-            instance.set_password(password)
-        if 'email' in validated_data:
-            email = self._validate_email(validated_data.pop('email'), instance)
-            instance.email = email
-        return super().update(instance, validated_data)
-
-    class Meta(UserSerializer.Meta):
-        extra_kwargs = {
-            'email': {'required': False, 'write_only': False},  # email необязателен при обновлении
-            'password': {'required': False, 'write_only': True},  # Пароль необязателен при обновлении
+            'email': {'required': False, 'write_only': False},
+            'password': {'required': False, 'write_only': True},
         }
--- src/server/api/v1/users/views.py
+++ src/server/api/v1/users/views.py
@@ -1,19 +1,20 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied
 
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 
 from server.apps.users.models import User
-from server.api.v1.users.serializer import UserSerializer, UserUpdateSerializer
+from server.api.v1.users.serializer import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API эндпоинт на crud пользователя.
     """
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    queryset = User.objects.all()
 
     @swagger_auto_schema(request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
@@ -43,7 +44,6 @@ class UserViewSet(viewsets.ModelViewSet):
         """
         Обновить пользователя.
         """
-        self.serializer_class = UserUpdateSerializer
         return super().update(request, *args, **kwargs)
 
     def get_permissions(self):
@@ -55,3 +55,10 @@ class UserViewSet(viewsets.ModelViewSet):
             'destroy': [IsAuthenticated],
         }
         return [permission() for permission in permissions.get(self.action, [AllowAny])]
+
+    def check_object_permissions(self, request, obj):
+        """
+        Проверить права доступа к объекту.
+        """
+        if obj.id != request.user.id:
+            raise PermissionDenied("Попытка изменить данные не своего аккаунта.")
